52acbd212559868f32446c461c05b59f0db25624
Matisse Page <matisse.page@epitech.eu>
2021-04-12 16:49:16 +0200

    add

--- job_dsl.groovy
+++ job_dsl.groovy
@@ -1,4 +1,16 @@
 folder('Tools') {
         displayName('Tools')
         description('Folder for miscellaneous tools.')
-      }
+}
+
+job('Tools/clone-repository') {
+    parameters {
+        stringParam('GIT_REPOSITORY_URL', '', 'Git URL of the repository to clone')
+    }
+    steps {
+        shell('git clone $GIT_REPOSITORY_URL')
+    }
+    wrappers {
+        preBuildCleanup()
+    }
+}
