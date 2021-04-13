0ea49dea17c1e1c86aa9c6c122fcd7543e50b967
Matisse Page <matisse.page@epitech.eu>
2021-04-13 10:22:40 +0200

    add comment

--- job_dsl.groovy
+++ job_dsl.groovy
@@ -13,32 +13,32 @@ job('Tools/clone-repository') {
         preBuildCleanup()
     }
 }
-job('Tools/SEED') {
-    parameters {
-        stringParam('GITHUB_NAME', '', 'GitHub repository owner/repo_name (e.g.: "EpitechIT31000/chocolatine")')
-        stringParam('DISPLAY_NAME', '', 'Display name for the job')
-    }
-    steps {
-        dsl {
-        text('''
-        job("Tools/$DISPLAY_NAME") {
-            wrappers {
-            preBuildCleanup()
-            }
-            scm {
-            github("$GITHUB_NAME")
-            }
-            triggers {
-            scm('H/1 * * * *')
-            }
-            steps {
-            shell('make fclean')
-            shell('make')
-            shell('make test')
-            shell('make clean')
-            }
-        }
-        '''.stripIndent())
-        }
-    }
-}
+// job('Tools/SEED') {
+//     parameters {
+//         stringParam('GITHUB_NAME', '', 'GitHub repository owner/repo_name (e.g.: "EpitechIT31000/chocolatine")')
+//         stringParam('DISPLAY_NAME', '', 'Display name for the job')
+//     }
+//     steps {
+//         dsl {
+//         text('''
+//         job("Tools/$DISPLAY_NAME") {
+//             wrappers {
+//             preBuildCleanup()
+//             }
+//             scm {
+//             github("$GITHUB_NAME")
+//             }
+//             triggers {
+//             scm('H/1 * * * *')
+//             }
+//             steps {
+//             shell('make fclean')
+//             shell('make')
+//             shell('make test')
+//             shell('make clean')
+//             }
+//         }
+//         '''.stripIndent())
+//         }
+//     }
+// }
