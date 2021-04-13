5d7b04642c86810d602c396cc4bf0c4ef3154b8d
Matisse Page <matisse.page@epitech.eu>
2021-04-13 09:50:28 +0200

    adding tools SEED

--- job_dsl.groovy
+++ job_dsl.groovy
@@ -2,7 +2,6 @@ folder('Tools') {
         displayName('Tools')
         description('Folder for miscellaneous tools.')
 }
-
 job('Tools/clone-repository') {
     parameters {
         stringParam('GIT_REPOSITORY_URL', '', 'Git URL of the repository to clone')
@@ -12,5 +11,11 @@ job('Tools/clone-repository') {
     }
     wrappers {
         preBuildCleanup()
+    }
+}
+job('Tools/SEED') {
+    parameters {
+        stringParam('GITHUB_NAME', '', 'GitHub repository owner/repo_name (e.g.: "EpitechIT31000/chocolatine")')
+        stringParam('DISPLAY_NAME', '', 'Display name for the job')
     }
 }
